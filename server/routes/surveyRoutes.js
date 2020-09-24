5712762cd720f098bfb58a727c28d7dfc6af54cc
Rafael <rafael.whatsthestory@gmail.com>
2020-09-24 12:33:10 -0300

    until class 201

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require("lodash");
+const { Path } = require("path-parser");
+const { URL } = require("url");
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
@@ -8,13 +11,57 @@ const Survey = mongoose.model("surveys");
 
 module.exports = (app) => {
   app.get(
-    "/api/surveys/thanks",
+    "/api/surveys/:surveyId/:choice",
     requireLogin,
     requireCredits,
     async (req, res) => {
       res.send("Thanks for voting!!!");
     }
   );
+
+  app.post("/api/surveys/webhooks", (req, res) => {
+    const p = new Path("/api/surveys/:surveyId/:choice");
+
+    // without lodash chain
+    // const events = _.map(req.body, ({ email, url }) => {
+    //   // console.log(p.test(pathname));
+    //   const match = p.test(p.test(pathname));
+    //   if (match) {
+    //     return { email, surveyId: match.surveyId, choice: match.choice };
+    //   }
+    // });
+    // const compactEvents = _.compact(events);
+    // const uniqueEvents = _.uniqBy(compactEvents, "email", "surveyId");
+    _.chain(req.body)
+      .map(() => {
+        const email = req.body.email;
+        const url = req.body.url;
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+      })
+      .compact()
+      .uniqBy("email", "surveyId")
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false },
+            },
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { "recipients.$.responded": true },
+            lastResponded: new Date(),
+          }
+        ).exec();
+      })
+      .value();
+
+    res.send({});
+  });
 
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
